834c552264ae6577bb225095b7bc48f4bc2f656e
Rukanda Faridsi <hey@rukanda.xyz>
2018-01-21 13:57:12 +0700

    Initial Commit

--- containers/Default.js
+++ containers/Default.js
@@ -1,28 +1,14 @@
-// @flow
-import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import App from '../components/App';
-import Header from '../components/Header';
-import ProjectInfo from '../components/ProjectInfo';
 
-type Props = {
-  title?: string,
-  url: UrlType,
-  children: React.Element<*>
-};
-
-const Default = (props: Props) => (
+const Default = props => (
   <App>
     <Helmet>
       <title>
-        {props.title && props.title !== ''
-          ? `${props.title} :: RAN! React . GraphQL . Next.js Toolkit`
-          : 'RAN! React . GraphQL . Next.js Toolkit'}
+        {props.title !== '' ? props.title : ''}
       </title>
     </Helmet>
-    <Header pathname={props.url.pathname} />
-    <ProjectInfo />
     {props.children}
   </App>
 );
